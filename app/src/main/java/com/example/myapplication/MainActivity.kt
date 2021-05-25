42ebaefc873d9486fe08ac0132ff5a6f7dd97675
ZXM250250 <2994250239@qq.com>
2021-05-25 21:02:20 +0800

    多人协作测试

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    val TAG="多人运动"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e(TAG,"合作开发测试")
+    }
+}
